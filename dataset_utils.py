386493d7d6da2c6674a90e0d23dbff6bed29bf45
alpertunga-bile <holy_celestine@hotmail.com>
2025-05-15 23:00:45 +0300

    dataset utils are added

---
+++ dataset_utils.py
@@ -0,0 +1,62 @@
+import polars as pl
+import os.path
+import threading
+
+
+def create_dataframe_from_list(infos: list, columns: list[str]) -> pl.DataFrame:
+    values: dict[str, list] = {column: [] for column in columns}
+
+    for info in infos:
+        for column in columns:
+            values[column].append(getattr(info, column))
+
+    return pl.DataFrame(values)
+
+
+def get_dataframe(dataset_path: str, columns: list[str]) -> pl.DataFrame:
+    pl.enable_string_cache()
+
+    if os.path.exists(dataset_path):
+        return pl.read_parquet(
+            dataset_path, columns=columns, use_pyarrow=True, memory_map=True
+        )
+
+    return pl.DataFrame()
+
+
+def add_postprocss_dataframe(
+    df: pl.DataFrame, values: list, unique_column: str, columns: list[str]
+) -> pl.DataFrame:
+    created_df = create_dataframe_from_list(infos=values, columns=columns)
+
+    new_df = pl.concat([df, created_df], how="vertical")
+    new_df = new_df.unique([unique_column], keep="last")
+    new_df = new_df.shrink_to_fit()
+
+    return new_df
+
+
+def print_save_dataframe(df: pl.DataFrame, parquet_filename: str, csv_filename: str):
+    df.write_parquet(parquet_filename)
+    df.write_csv(csv_filename)
+
+    print(f"Total rows of dataframe: {len(df)}")
+    print(df.head(5))
+
+
+dataset_lock = threading.Lock()
+
+
+def add_save_dataset(
+    dataset_path: str,
+    columns: list[str],
+    values: list,
+    unique_key: str,
+    parquet_filename: str,
+    csv_filename: str,
+):
+    with dataset_lock:
+        df = get_dataframe(dataset_path, columns)
+        df = add_postprocss_dataframe(df, values, unique_key, columns)
+
+        print_save_dataframe(df, parquet_filename, csv_filename)
