c4a5f5a6063b8f79f42016e6e7a88750a48f7488
alpertunga-bile <holy_celestine@hotmail.com>
2025-05-15 23:01:08 +0300

    request utils are added

---
+++ request_utils.py
@@ -0,0 +1,50 @@
+import concurrent.futures
+import time
+import random
+import requests
+
+from typing import Any
+from configs import user_agents
+
+
+def into_chunks(arr, chunk_size):
+    for i in range(0, len(arr), chunk_size):
+        yield arr[i : i + chunk_size]
+
+
+def parallel_execute(urls: list[str], function):
+    results = []
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=len(urls)) as executor:
+        futures = [executor.submit(function, url) for url in urls]
+
+        for future in concurrent.futures.as_completed(futures):
+            results.extend(future.result())
+
+    return results
+
+
+def get_json(url: str) -> Any | None:
+    time.sleep(random.random() * 1.5)
+
+    headers = {"User-Agent": user_agents[random.randint(0, len(user_agents) - 1)]}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        for _ in range(3):
+            time.sleep(random.random() * 2.0)
+
+            headers = {
+                "User-Agent": user_agents[random.randint(0, len(user_agents) - 1)]
+            }
+
+            response = requests.get(url, headers=headers)
+
+            if response.status_code == 200:
+                break
+
+    if response.status_code != 200:
+        return None
+
+    return response.json()
