4d9c060fcd6c12073aa9ff5ad4e64e158e1b67fe
alpertunga-bile <holy_celestine@hotmail.com>
2025-06-14 18:32:02 +0300

    get_output function is updated

--- output/utils.py
+++ output/utils.py
@@ -1,7 +1,6 @@
 from .media import MediaOutput
 import concurrent.futures
 import tqdm
-import request_utils
 
 
 def fill_output(value) -> MediaOutput | None:
@@ -11,29 +10,19 @@ def fill_output(value) -> MediaOutput | None:
     return output_elem if ret_val else None
 
 
-def get_output(values: list, function) -> list[MediaOutput]:
-    results = []
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=len(values)) as executor:
-        futures = [executor.submit(function, value) for value in values]
-
-        for future in concurrent.futures.as_completed(futures):
-            res = future.result()
-
-            if res is not None:
-                results.append(res)
-
-    return results
-
-
 def get_outputs(media_items) -> list[MediaOutput]:
     outputs: list[MediaOutput] = []
 
-    with tqdm.tqdm(
-        total=len(media_items), desc="Filling Items", position=3, leave=False
-    ) as pbar:
-        for chunk in request_utils.into_chunks(media_items, 128):
-            outputs.extend(get_output(chunk, fill_output))
-            pbar.update(len(chunk))
+    with tqdm.tqdm(total=len(media_items), desc="Filling Items") as pbar:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=128) as executor:
+            futures = [executor.submit(fill_output, item) for item in media_items]
+
+            for future in concurrent.futures.as_completed(futures):
+                res = future.result()
+
+                if res is not None:
+                    outputs.append(res)
+
+                pbar.update(1)
 
     return outputs
